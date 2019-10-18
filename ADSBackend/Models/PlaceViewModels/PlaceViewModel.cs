79ac8bfdf964c8260da5e2e1ebe2b3e39fa2aeda
Dylan McIntyre <dylanrmcintyre@gmail.com>
2019-10-18 11:15:46 -0400

    Added place view

---
+++ ADSBackend/Models/PlaceViewModels/PlaceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSBackend.Models.PlaceViewModels
+{
+    public class PlaceViewModel
+    {
+        [Key]
+        public int PlaceId;
+
+        [Required]
+        [Display(Name = "Place Name")]
+        public String Name;
+
+        [Required]
+        [Display(Name = "Location Name")]
+        public String Location;
+    }
+}
