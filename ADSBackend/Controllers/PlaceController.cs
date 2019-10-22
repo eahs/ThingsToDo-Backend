dd0dadc5f2dc1765bd2677667a7112f346d6b3ff
Dylan McIntyre <dylanrmcintyre@gmail.com>
2019-10-22 11:20:15 -0400

    Added place controller; developed place view

---
+++ ADSBackend/Controllers/PlaceController.cs
@@ -0,0 +1,28 @@
+﻿using ADSBackend.Data;
+using ADSBackend.Models.Identity;
+using ADSBackend.Models.ManageViewModels;
+using ADSBackend.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Controllers
+{
+    [Authorize]
+    public class PlaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
