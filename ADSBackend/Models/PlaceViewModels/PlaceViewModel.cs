cea94a91e193c60089a22d883df23e48b0a1e66d
Dylan McIntyre <dylanrmcintyre@gmail.com>
2019-10-30 10:48:16 -0400

    Updated database

--- ADSBackend/Models/PlaceViewModels/PlaceViewModel.cs
+++ ADSBackend/Models/PlaceViewModels/PlaceViewModel.cs
@@ -15,5 +15,16 @@ namespace ADSBackend.Models.PlaceViewModels
         [Required]
         [Display(Name = "Location Name")]
         public String Location { get; set; }
+
+        [Required]
+        [Display(Name = "Activity Name")]
+        public String Activity { get; set; }
+
+        [Display(Name = "Activity Description")]
+        public String Description { get; set; }
+
+        [Required]
+        [Display(Name = "Location Address")]
+        public String Address { get; set; }
     }
 }
